57bb64ebd280bf831ea249b4edd58b78add79b5b
nvantienen <nathalievantienen@live.nl>
2019-05-29 13:44:24 +0200

    blowForce!

--- Assets/airBoxTrigger.cs
+++ Assets/airBoxTrigger.cs
@@ -2,11 +2,24 @@
 
 public class airBoxTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public float blowForce =1f;
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             Debug.Log("woooosh c:");
         }  
+        //other.gameObject.transform.Translate(Vector3.up * blowForce * Time.deltaTime, Space.World);
+
+   
+
+        if(other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * blowForce* Time.deltaTime * 100);
+            Debug.Log("werk pls");
+        }
+
     }
 }
